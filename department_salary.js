cec85e547130fb613f75fea0265793d23ac60a4f
ZhengranHuang <Zhengran@usf.edu>
2024-09-27 22:43:20 -0400

    wrongoutput

--- department_salary.js
+++ department_salary.js
@@ -51,3 +51,24 @@ subordinates: []
 }
 ]
 };
+//Task2:Create a Recursive Function to Calculate Total Salary for a Department
+function calculateDepartmentSalary(department){
+let totalSalary;
+let totalSalaryinsubordinates;
+let realtotalSalary;
+const departs = company.find(dn => dn.departmentName === department);
+if (departs.subordinates.length === 0 && departs){
+totalSalary += employee.salary;
+return console.log(`${department} total Salary "${totalSalary}"`)
+};
+if (departs.subordinates.length > 0 && departs){
+employee.subordinates.forEach(subordinate =>{totalSalaryinsubordinates+= subordinate.salary});
+totalSalary += employee.salary;
+realtotalSalary = totalSalaryinsubordinates + totalSalary;
+return console.log(`${department} total Salary "${realtotalSalary}"`)
+}
+if(!departs){
+console.log(`${department} is Error.`)
+}
+}
+calculateDepartmentSalary('Sales')
